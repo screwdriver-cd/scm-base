cf9e553ea19494f465e62444d023a8c1a923c0a9
Noah Katzman <nkatzman@yahoo-inc.com>
2016-08-15 15:09:32 -0700

    adding in initial functions that we use in the github helper class

---
+++ index.js
@@ -0,0 +1,128 @@
+'use strict';
+/* eslint-disable no-underscore-dangle */
+const Joi = require('joi');
+
+class ScmBase {
+    /**
+     * Constructor for Scm
+     * @method constructor
+     * @param  {Object}    config Configuration
+     * @return {ScmBase}
+     */
+    constructor(config) {
+        this.config = config;
+    }
+
+    /**
+     * Reload configuration
+     * @method configure
+     * @param  {Object}     config      Configuration
+     */
+    configure(config) {
+        this.config = config;
+    }
+
+    /**
+     * Format the scmUrl for the specific source control
+     * @method formatScmUrl
+     * @param {String}    scmUrl        Scm Url to format properly
+     */
+    formatScmUrl() {
+        throw new Error('formatScmUrl not implemented');
+    }
+
+    /**
+     * Get a users permissions on a repository
+     * @method getPermissions
+     * @param  {Object}   config            Configuration
+     * @param  {String}   config.scmUrl     The scmUrl to get permissions on
+     * @param  {String}   config.user       The user to get pemissions for
+     * @param  {String}   config.token      The token used to authenticate to the SCM
+     * @return {Promise}
+     */
+    getPermissions(config) {
+        const result = Joi.validate(config, {
+            scmUrl: Joi.string().required(),
+            user: Joi.string().required(),
+            token: Joi.string().required()
+        });
+
+        if (result.error) {
+            return new Promise((resolve, reject) => {
+                process.nextTick(() => reject(result.error));
+            });
+        }
+
+        return this._getPermissions(config);
+    }
+
+    _getPermissions() {
+        return new Promise((resolve, reject) => {
+            process.nextTick(() => reject('not implemented'));
+        });
+    }
+
+    /**
+     * Get a commit sha for a specific repo#branch
+     * @method getCommitSha
+     * @param  {Object}   config            Configuration
+     * @param  {String}   config.scmUrl     The scmUrl to get commit sha of
+     * @param  {String}   config.token      The token used to authenticate to the SCM
+     * @return {Promise}
+     */
+    getCommitSha(config) {
+        const result = Joi.validate(config, {
+            scmUrl: Joi.string().required(),
+            token: Joi.string().required()
+        });
+
+        if (result.error) {
+            return new Promise((resolve, reject) => {
+                process.nextTick(() => reject(result.error));
+            });
+        }
+
+        return this._getCommitSha(config);
+    }
+
+    _getCommitSha() {
+        return new Promise((resolve, reject) => {
+            process.nextTick(() => reject('not implemented'));
+        });
+    }
+
+    /**
+     * Update the commit status for a given repo and sha
+     * @method get
+     * @param  {Object}   config              Configuration
+     * @param  {String}   config.scmUrl       The scmUrl to get permissions on
+     * @param  {String}   config.sha          The sha to apply the status to
+     * @param  {String}   config.buildStatus  The build status used for figuring out the commit status to set
+     * @param  {String}   config.token        The token used to authenticate to the SCM
+     * @return {Promise}
+     */
+    updateCommitStatus(config) {
+        const result = Joi.validate(config, {
+            scmUrl: Joi.string().required(),
+            sha: Joi.string().required(),
+            buildStatus: Joi.string().required(),
+            token: Joi.string().required()
+        });
+
+        if (result.error) {
+            return new Promise((resolve, reject) => {
+                process.nextTick(() => reject(result.error));
+            });
+        }
+
+        return this._updateCommitStatus(config);
+    }
+
+    _updateCommitStatus() {
+        return new Promise((resolve, reject) => {
+            process.nextTick(() => reject('not implemented'));
+        });
+    }
+}
+
+module.exports = ScmBase;
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,124 @@
 'use strict';
+/* eslint-disable no-underscore-dangle */
 const assert = require('chai').assert;
 
 describe('index test', () => {
-    it('fails', () => {
-        assert.isTrue(false);
+    let instance;
+    let ScmBase;
+
+    beforeEach(() => {
+        /* eslint-disable global-require */
+        ScmBase = require('../index');
+        /* eslint-enable global-require */
+
+        instance = new ScmBase({ foo: 'bar' });
+    });
+
+    afterEach(() => {
+        instance = null;
+    });
+
+    it('can create an scm base class', () => {
+        assert.instanceOf(instance, ScmBase);
+    });
+
+    describe('configure', () => {
+        it('has a configure method', () => {
+            assert.isFunction(instance.configure);
+            instance.configure({});
+        });
+    });
+
+    describe('formatScmUrl', () => {
+        it('throws an error', () => {
+            assert.throws(() => instance.formatScmUrl('foo'), 'formatScmUrl not implemented');
+        });
+    });
+
+    describe('getPermissons', () => {
+        it('returns error when invalid config object', () => instance.getPermissions({})
+            .then(() => {
+                assert(false, 'you will never get dis');
+            })
+            .catch(err => {
+                assert.isOk(err, 'Error should be returned');
+                assert.equal(err.name, 'ValidationError');
+            })
+        );
+
+        it('returns not implemented', () => {
+            const config = {
+                scmUrl: 'foo',
+                user: 'bar',
+                token: 'token'
+            };
+
+            instance.getPermissions(config)
+              .then(() => {
+                  assert(false, 'you will never get dis');
+              })
+              .catch(err => {
+                  assert.isOk(err, 'Error should be returned');
+                  assert.equal(err.message, 'not implemented');
+              });
+        });
+    });
+
+    describe('getCommitSha', () => {
+        it('returns error when invalid config object', () => instance.getCommitSha({})
+            .then(() => {
+                assert(false, 'you will never get dis');
+            })
+            .catch(err => {
+                assert.isOk(err, 'Error should be returned');
+                assert.equal(err.name, 'ValidationError');
+            })
+        );
+
+        it('returns not implemented', () => {
+            const config = {
+                scmUrl: 'foo',
+                token: 'token'
+            };
+
+            instance.getCommitSha(config)
+              .then(() => {
+                  assert(false, 'you will never get dis');
+              })
+              .catch(err => {
+                  assert.isOk(err, 'Error should be returned');
+                  assert.equal(err.message, 'not implemented');
+              });
+        });
+    });
+
+    describe('updateCommitStatus', () => {
+        it('returns error when invalid config object', () => instance.updateCommitStatus({})
+            .then(() => {
+                assert(false, 'you will never get dis');
+            })
+            .catch(err => {
+                assert.isOk(err, 'Error should be returned');
+                assert.equal(err.name, 'ValidationError');
+            })
+        );
+
+        it('returns not implemented', () => {
+            const config = {
+                scmUrl: 'foo',
+                sha: 'sha1',
+                buildStatus: 'stating',
+                token: 'token'
+            };
+
+            instance.updateCommitStatus(config)
+              .then(() => {
+                  assert(false, 'you will never get dis');
+              })
+              .catch(err => {
+                  assert.isOk(err, 'Error should be returned');
+                  assert.equal(err.message, 'not implemented');
+              });
+        });
     });
 });
