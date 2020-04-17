5aaaed9159c4fa09700f0a117fa090a6dfe454f9
Keisuke Kumada <kumada626@users.noreply.github.com>
2020-04-18 04:07:56 +0900

    fix(2029): Add prSource to prInfo return value (#78)

--- index.js
+++ index.js
@@ -455,7 +455,8 @@ class ScmBase {
                 url: Joi.reach(dataSchema.core.scm.pr, 'url'),
                 userProfile: Joi.reach(dataSchema.core.scm.pr, 'userProfile'),
                 baseBranch: Joi.reach(dataSchema.core.scm.pr, 'baseBranch'),
-                mergeable: Joi.boolean().allow(null)
+                mergeable: Joi.boolean().allow(null),
+                prSource: Joi.string().optional()
             })));
     }
 
